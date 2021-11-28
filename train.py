713325e6e9f980a78f35db419116db1aa61261dc
hanif <dev.har07@gmail.com>
2021-11-28 16:34:35 +0700

    use InceptionV3 without pretrained weight init

--- train.py
+++ train.py
@@ -102,7 +102,7 @@ else:
     train_dataset, val_dataset = load_split_train_test(dataset_dir, bs=train_batch_size, valid_bs=train_batch_size, valid_size=0.2, balanced=balance)
 
 # Base model InceptionV3 with global average pooling.
-model = torchvision.models.inception_v3(pretrained=True, progress=True, aux_logits=False)
+model = torchvision.models.inception_v3(pretrained=False, progress=True, aux_logits=False)
 
 # Reset the layer with the same amount of neurons as labels.
 num_ftrs = model.fc.in_features
